a467abc357c6b6a8c3dd2041e0c56e8f128d34a0
unknown <zbignevus@gmail.com>
2018-10-23 06:16:20 +0300

    Updates

---
+++ precache-manifest.44cbcf8a72c4395afc1e092d71c52a1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/placard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "23123c8c739f7ae2a8a5",
+    "url": "/placard/static/css/main.d524adbd.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/placard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "db70b76119d61571be2e",
+    "url": "/placard/static/js/1.db70b761.chunk.js"
+  },
+  {
+    "revision": "f88d6e1471c6f5790dfb",
+    "url": "/placard/static/js/runtime~main.f88d6e14.js"
+  },
+  {
+    "revision": "23123c8c739f7ae2a8a5",
+    "url": "/placard/static/js/main.23123c8c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/placard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/placard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/placard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "db70b76119d61571be2e",
+    "url": "/placard/static/css/1.a380b4ae.chunk.css"
+  },
+  {
+    "revision": "b88ce72f612d67d0cf82bb69c0a37519",
+    "url": "/placard/index.html"
+  }
+];
